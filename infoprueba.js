8ed0f9166c419859ae090ca076779f75fa8c65ac
MATHEWGAMERSTANLEY <88214334+MATHEWGAMERSTANLEY@users.noreply.github.com>
2021-07-30 17:14:22 -0500

    Add files via upload

---
+++ infoprueba.js
@@ -0,0 +1,33 @@
+function suma() {
+    var num1 = parseInt(prompt("introduce un numero de suma"))
+    var num2 = parseInt(prompt("introduce un numero"))
+
+    var resultado = num1 + num2;
+    alert(resultado)
+}
+
+
+function resta() {
+    var num1 = parseInt(prompt("introduce un numero para restar"))
+    var num2 = parseInt(prompt("introduce un numero"))
+
+    var resultado = num1 - num2;
+    alert(resultado)
+}
+
+function multiplicacion() {
+    var num1 = parseInt(prompt("introduce un numero para multiplicar"))
+    var num2 = parseInt(prompt("introduce un numero"))
+
+    var resultado = num1 * num2;
+    alert(resultado)
+}
+
+
+function division() {
+    var num1 = parseInt(prompt("introduce un numero para dividir"))
+    var num2 = parseInt(prompt("introduce un numero"))
+
+    var resultado = num1 / num2;
+    alert(resultado)
+}
